7b35234b30bbaf0054d0514a49de08145002fc62
Okhrimova <okhrimova@bk.ru>
2022-05-11 00:10:55 +0300

    исправил дз2

--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,42 @@ public class Main {
         float[] arr = {1.57f, 7.654f, 9.986f};
 
 
-        int [] arr1 = {25, 45, 54, 87};
+        int [] arr1 = {25, 45, 54};
 
         //Задание 2
-        System.out.println(weight[0] + ", " + weight[1] + ", " + weight[2]);
-        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
-        System.out.println(arr1[0] + ", " + arr1[1] + ", " + arr1[2]);
+        for (int i = 0; i < weight.length; i++) {
+            if (i < weight.length - 1)
+                System.out.println(weight[i] + ",");
+            else System.out.println(weight[i]);
+        }
+        System.out.println();
 
-        //Задание3
-        System.out.println(weight[2] + ", " + weight[1] + ", " + weight[0]);
-        System.out.println(arr[2] + ", " + arr[1] + ", " + arr[0]);
-        System.out.println(arr1[2] + ", " + arr1[1] + ", " + arr1[0]);
+
+        for (int i = 0; i <arr.length; i++) {
+            if (i < arr.length - 1)
+                System.out.println(arr[i] + ",");
+            else System.out.println(arr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i <arr1.length; i++) {
+            if (i < arr1.length - 1)
+                System.out.println(arr1[i] + ",");
+            else System.out.println(arr1[i]);
+        }
+        System.out.println();
 
         //Задание4
 
         for (int i = 0; i<weight.length; i++ ){
             if (weight[i]%2 != 0){
-                System.out.println(weight[i] + 1);
                 weight[i]++;
+                System.out.println(weight[i]);
             }
 
+            if (i!=weight.length)
+                System.out.println(weight[i] + " ,");
+            else System.out.println(weight[i]);
         }
 
 

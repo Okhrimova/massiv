43c241e312fed31480257681c32b4db061d7005c
Okhrimova <okhrimova@bk.ru>
2022-05-10 17:16:58 +0300

    выполнил дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] weight = new int[12];
+        weight [0] = 1;
+        weight [1] = 2;
+        weight [2] = 3;
+        int weight1 = weight[0];
+        int weights2 = weight [1];
+        int weights3 = weight [2];
+        System.out.println(weight1);
+        System.out.println(weights2);
+        System.out.println(weights3);
+
+        float[] arr = {1.57f, 7.654f, 9.986f};
+
+
+        int [] i1 = {25, 45, 54, 87};
+
+        //Задание 2
+        System.out.println(weight[0] + ", " + weight[1] + ", " + weight[2]);
+        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
+        System.out.println(i1[0] + ", " + i1[1] + ", " + i1[2]);
+
+        //Задание3
+        System.out.println(weight[2] + ", " + weight[1] + ", " + weight[0]);
+        System.out.println(arr[2] + ", " + arr[1] + ", " + arr[0]);
+        System.out.println(i1[2] + ", " + i1[1] + ", " + i1[0]);
+
+        //Задание4
+
+
     }
 }
